eb484d9a4f97fd128c38e63402db3a8c2051f5b9
XIV <lancelot.jouault@outlook.com>
2025-05-16 00:10:29 +0200

    Developement du server avec les premiers test de multithread

---
+++ src/GameApp.java
@@ -0,0 +1,38 @@
+package src;
+
+import src.Game.Client;
+import src.Server.Server;
+
+public class GameApp {
+
+    private Thread serverThread;
+
+    public static void main(String[] args) {
+
+        // Lancer le serveur dans un thread
+        Thread serveurThread = new Thread(new Server());
+        serveurThread.start();
+
+
+
+        Thread clientlocal = new Thread(new Client("localhost"));
+        clientlocal.start();
+        try {
+            Thread.sleep(1000); // évite de boucler à vide
+        } catch (InterruptedException e) {
+
+        }
+
+        Thread clientlocal2 = new Thread(new Client("localhost"));
+        clientlocal2.start();
+
+        // Bloque le main thread
+        while (true) {
+            try {
+                Thread.sleep(1000); // évite de boucler à vide
+            } catch (InterruptedException e) {
+                break;
+            }
+        }
+    }
+}
